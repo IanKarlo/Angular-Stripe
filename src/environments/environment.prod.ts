e79de8e6b15ee919be18086ef492aab31739d4e1
IanKarlo <iankarlots@gmail.com>
2021-04-01 19:14:53 -0300

    Add api key and url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  route: 'https://stripe-angular-backend.herokuapp.com', //your backend url here
+  stripe_public_key: 'pk_test_51IYbkSL1TDIZmDd4svs6YVNL90SU7u6puOSjLR30Eit9E3WhuusTWI3Tw4kdMyLeBFdWLSUbdqvImAdGiNw4IC1o00l1NfOMnQ', //your stripe public key here
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  route: '', //your backend url here
-  stripe_public_key: '', //your stripe public key here
+  route: 'https://stripe-angular-backend.herokuapp.com', //your backend url here
+  stripe_public_key: 'pk_test_51IYbkSL1TDIZmDd4svs6YVNL90SU7u6puOSjLR30Eit9E3WhuusTWI3Tw4kdMyLeBFdWLSUbdqvImAdGiNw4IC1o00l1NfOMnQ', //your stripe public key here
 };
 
 /*
