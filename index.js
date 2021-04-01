3cdf7a1424b8dd4ddae9ead5fad760c1e04d5179
IanKarlo <iankarlots@gmail.com>
2021-04-01 19:47:51 -0300

    Index,js adjust

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/AngularStripe'));
+app.use(express.static(__dirname + '/dist/angular-stripe'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/AngularStripe/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angular-stripe/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+  console.log("App running on port 8080.");
+});
